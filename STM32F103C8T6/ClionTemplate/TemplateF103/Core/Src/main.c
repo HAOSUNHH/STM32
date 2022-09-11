46901d920aa652452fccb262d6f594f2fb0b0a5d
Yu-1120 <1186893249@qq.com>
2022-09-11 22:04:44 +0800

    update

--- STM32F103C8T6/ClionTemplate/TemplateF103/Core/Src/main.c
+++ STM32F103C8T6/ClionTemplate/TemplateF103/Core/Src/main.c
@@ -5,45 +5,13 @@
 
 
 
-
-
-
-#include "stm32f10x.h"
-
-void GPIO_Configuration(void);
-
-void Delay(uint32_t nCount)
-{
-    for(; nCount != 0; nCount--);
-}
-
 int main(void)
 {   int TRUE=1;
-    GPIO_Configuration();
     uart_init(115200);
     delay_init();
-    while(TRUE)
-    {
+    while(TRUE){
         USART_SEND(USART1,"Hello World!\n");
         delay_ms(1000);
-        //***********************************
-        PCout(13)=1;
-        Delay(0xfffff);
-        PCout(13)=0;
-        Delay(0xfffff);
-
     }
+    return 0;
 }
-void GPIO_Configuration(void)
-{
-    GPIO_InitTypeDef GPIO_InitStructure;
-
-    RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC , ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-}
-
-
-
--- STM32F103C8T6/ClionTemplate/ex/USER/Src/main.c
+++ STM32F103C8T6/ClionTemplate/ex/USER/Src/main.c
@@ -6,29 +6,30 @@
    u32 i=0;
    for(;i<count;i++);
  }
-void GPIO_Configuration(void)
-{
-    GPIO_InitTypeDef GPIO_InitStructure;
 
-    RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC , ENABLE);
-
-    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_13;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-}
-
+ int TRUE=1;
  int main(void)
- {
-     GPIO_Configuration();
+ {	
+  GPIO_InitTypeDef  GPIO_InitStructure;
+	 
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|
+  RCC_APB2Periph_GPIOE, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+  GPIO_SetBits(GPIOB,GPIO_Pin_5);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
+  GPIO_SetBits(GPIOE,GPIO_Pin_5);
 
-  while(1)
+  while(TRUE)
 	{
-	    GPIO_ResetBits(GPIOC,GPIO_Pin_13);
-//	    GPIO_SetBits(GPIOE,GPIO_Pin_5);
-//		Delay(1000000);
-//		GPIO_SetBits(GPIOC,GPIO_Pin_13);
-//		GPIO_ResetBits(GPIOE,GPIO_Pin_5);
-//		Delay(1000000);
+	    GPIO_ResetBits(GPIOB,GPIO_Pin_5);
+	    GPIO_SetBits(GPIOE,GPIO_Pin_5);
+		Delay(3000000);
+		GPIO_SetBits(GPIOB,GPIO_Pin_5);
+		GPIO_ResetBits(GPIOE,GPIO_Pin_5);
+		Delay(3000000);
 	}
  }
