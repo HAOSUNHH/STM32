5bbdb101d9de1a46d07ec66a2933a87d9dca996d
Yu-1120 <1186893249@qq.com>
2022-09-12 23:32:36 +0800

    update

---
+++ STM32F103C8T6/STM32F103C8T6-LED/cmake-build-debug/CMakeFiles/STM32F103C8T6-LED.elf.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/startup/startup_stm32f103xb.s" "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/cmake-build-debug/CMakeFiles/STM32F103C8T6-LED.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Core/Src/gpio.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/gpio.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Core/Src/main.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/main.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Core/Src/stm32f1xx_it.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Core/Src/syscalls.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/syscalls.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Core/Src/system_stm32f1xx.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "E:/GitHub/STM32/STM32F103C8T6/STM32F103C8T6-LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/STM32F103C8T6-LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
