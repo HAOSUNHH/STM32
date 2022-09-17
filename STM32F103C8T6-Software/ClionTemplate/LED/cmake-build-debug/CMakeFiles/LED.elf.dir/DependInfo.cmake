d5d987c1be0e9b9f5c425598bd3a26176a196ef6
Yu-1120 <1186893249@qq.com>
2022-09-17 14:53:16 +0800

    update

---
+++ STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/DependInfo.cmake
@@ -0,0 +1,69 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/startup/startup_stm32f103xb.s" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Core/Src/gpio.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Core/Src/gpio.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Core/Src/main.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Core/Src/main.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Core/Src/stm32f1xx_hal_msp.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Core/Src/stm32f1xx_it.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Core/Src/syscalls.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Core/Src/syscalls.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Core/Src/system_stm32f1xx.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "E:/GitHub/STM32/STM32F103C8T6-Software/ClionTemplate/LED/cmake-build-debug/CMakeFiles/LED.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
